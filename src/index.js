74b7829c5c4999a5faae5eea8c6bf6a06f4bfd52
Quyen Hoang <21020787@vnu.edu.vn>
2023-04-26 21:49:06 +0700

    changing handlebars to hbs extension

--- src/index.js
+++ src/index.js
@@ -19,7 +19,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Template engine
-app.engine('handlebars', engine());
+app.engine('hbs', engine({
+    extname: '.hbs'
+}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
